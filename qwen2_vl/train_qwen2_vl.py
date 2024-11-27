20265d90cf92b27c8d5f2b69c460f43bd4637057
Ze-Yi LIN <58305964+Zeyi-Lin@users.noreply.github.com>
2024-11-27 21:21:34 +0800

    Update train_qwen2_vl.py

--- qwen2_vl/train_qwen2_vl.py
+++ qwen2_vl/train_qwen2_vl.py
@@ -24,33 +24,67 @@ def process_func(example):
     conversation = example["conversations"]
     input_content = conversation[0]["value"]
     output_content = conversation[1]["value"]
-
-    instruction = tokenizer(
-        f"<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\n{input_content}<|im_end|>\n<|im_start|>assistant\n",
-        add_special_tokens=False,
+    file_path = input_content.split("<|vision_start|>")[1].split("<|vision_end|>")[0]  # 获取图像路径
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": f"{file_path}",
+                    "resized_height": 280,
+                    "resized_width": 280,
+                },
+                {"type": "text", "text": "COCO Yes:"},
+            ],
+        }
+    ]
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )  # 获取文本
+    image_inputs, video_inputs = process_vision_info(messages)  # 获取数据数据（预处理过）
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
     )
+    inputs = {key: value.tolist() for key, value in inputs.items()} #tensor -> list,为了方便拼接
+    instruction = inputs
+
     response = tokenizer(f"{output_content}", add_special_tokens=False)
+
+
     input_ids = (
-        instruction["input_ids"] + response["input_ids"] + [tokenizer.pad_token_id]
-    )
-    attention_mask = instruction["attention_mask"] + response["attention_mask"] + [1]
-    labels = (
-        [-100] * len(instruction["input_ids"])
-        + response["input_ids"]
-        + [tokenizer.pad_token_id]
+            instruction["input_ids"][0] + response["input_ids"] + [tokenizer.pad_token_id]
     )
 
+    attention_mask = instruction["attention_mask"][0] + response["attention_mask"] + [1]
+    labels = (
+            [-100] * len(instruction["input_ids"][0])
+            + response["input_ids"]
+            + [tokenizer.pad_token_id]
+    )
     if len(input_ids) > MAX_LENGTH:  # 做一个截断
         input_ids = input_ids[:MAX_LENGTH]
         attention_mask = attention_mask[:MAX_LENGTH]
         labels = labels[:MAX_LENGTH]
 
-    return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels}
+    input_ids = torch.tensor(input_ids)
+    attention_mask = torch.tensor(attention_mask)
+    labels = torch.tensor(labels)
+    inputs['pixel_values'] = torch.tensor(inputs['pixel_values'])
+    inputs['image_grid_thw'] = torch.tensor(inputs['image_grid_thw']).squeeze(0)  #由（1,h,w)变换为（h,w）
+    return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels,
+            "pixel_values": inputs['pixel_values'], "image_grid_thw": inputs['image_grid_thw']}
 
 
 def predict(messages, model):
     # 准备推理
-    text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
     image_inputs, video_inputs = process_vision_info(messages)
     inputs = processor(
         text=[text],
@@ -69,7 +103,7 @@ def predict(messages, model):
     output_text = processor.batch_decode(
         generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
     )
-
+    
     return output_text[0]
 
 
@@ -120,6 +154,7 @@ args = TrainingArguments(
     per_device_train_batch_size=4,
     gradient_accumulation_steps=4,
     logging_steps=10,
+    logging_first_step=5,
     num_train_epochs=2,
     save_steps=100,
     learning_rate=1e-4,
@@ -127,7 +162,7 @@ args = TrainingArguments(
     gradient_checkpointing=True,
     report_to="none",
 )
-
+        
 # 设置SwanLab回调
 swanlab_callback = SwanLabCallback(
     project="Qwen2-VL-finetune",
@@ -180,20 +215,20 @@ for item in test_dataset:
     input_image_prompt = item["conversations"][0]["value"]
     # 去掉前后的<|vision_start|>和<|vision_end|>
     origin_image_path = input_image_prompt.split("<|vision_start|>")[1].split("<|vision_end|>")[0]
-
+    
     messages = [{
         "role": "user", 
         "content": [
             {
-                "type": "image", 
-                "image": origin_image_path
+            "type": "image", 
+            "image": origin_image_path
             },
             {
-                "type": "text",
-                "text": "COCO Yes:"
+            "type": "text",
+            "text": "COCO Yes:"
             }
         ]}]
-
+    
     response = predict(messages, val_peft_model)
     messages.append({"role": "assistant", "content": f"{response}"})
     print(messages[-1])
